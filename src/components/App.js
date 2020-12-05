136ab4df865a05c230b0f61ddcf13966ff167595
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-05 21:02:54 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <h1 data-ns-test="project-name"> Online Food Selling App </h1>
+      <dl data-ns-test="project-description">
+        <dd>Selling Sea food, fishes and meats in online </dd>
+      </dl>
+    </>
+  );
+};
 
 export default App;
